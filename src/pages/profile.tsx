7e436ace492825e83a6d9a696843b46b3ce56ff5
Cynthia <cynthiaren2000@gmail.com>
2020-02-20 23:45:36 -0500

    bonus feature: user can check in and attend workshop

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -7,6 +7,7 @@ import AttendeeAvatar from '../components/attendeeAvatar';
 import ProfileTable from '../components/profileTable';
 import ActionButton from '../components/actionButton';
 import { ErrorMessage, InfoMessage } from '../components/messageDisplay';
+import { AttendeeProfile } from '../types/attendeeProfile';
 
 // Footer for check-in button using styled component
 const Footer = styled.h1`
@@ -20,7 +21,39 @@ const Footer = styled.h1`
 `;
 
 /**
- * Controller Component for getting attendee profile and render page, 
+ * Component for displaying full attendee profile and
+ * handles check in functionality
+ * 
+ * @param {AttendeeProfile} attendee the attendee with full profile
+ * 
+ * @return {React.FC}
+ */
+
+interface ProfileProps {
+  attendee: AttendeeProfile;
+}
+
+const Profile: React.FC<ProfileProps> = ({ attendee }) => {
+  const [hasCheckedIn, setCheckIn] = React.useState(attendee.checked_in);
+
+  const handleCheckIn = () => {
+    setCheckIn(!hasCheckedIn);
+  };
+
+  const buttonText = hasCheckedIn ? "Checked In" : "Check In";
+  return (
+    <React.Fragment>
+      <AttendeeAvatar name={attendee.name} url={attendee.profile_pic} type={attendee.type} checkedIn={hasCheckedIn}/>
+      <ProfileTable attendee={attendee} />
+      <Footer>
+        <ActionButton variant="contained" color="primary" disabled={hasCheckedIn} onClick={handleCheckIn}>{buttonText}</ActionButton>
+      </Footer>
+    </React.Fragment>
+  );
+}
+
+/**
+ * Page Component for getting attendee profile and render page, 
  * also catches different types of error and displays error message accordingly.
  * 
  * @param {string} url service url
@@ -43,17 +76,7 @@ const ProfilePage: React.FC<Props> = ({ url }) => {
     if (service.payload === null) {
       return <ErrorMessage>No attendee profile found. Please contact support.</ErrorMessage>;
     } else {
-      const attendee = service.payload;
-      const buttonText = attendee.checked_in ? "Checked In" : "Check In";
-      return (
-        <React.Fragment>
-          <AttendeeAvatar name={attendee.name} url={attendee.profile_pic} type={attendee.type} checkedIn={attendee.checked_in}/>
-          <ProfileTable attendee={attendee} />
-          <Footer>
-            <ActionButton variant="contained" color="primary" disabled={attendee.checked_in}>{buttonText}</ActionButton>
-          </Footer>
-        </React.Fragment>
-      );
+      return <Profile attendee={service.payload} />
     }
   }
 
